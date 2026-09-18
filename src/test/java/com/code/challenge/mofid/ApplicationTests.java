b58502b6f0455c5ea5079d64eaf51a1abc27b9c4
Rahimzadeh Naeim <Rahimzadeh.Naeim@tosan-solutions.com>
2026-09-18 12:06:00 +0330

    Phase 0 - Project Skeleton

---
+++ src/test/java/com/code/challenge/mofid/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.challenge.mofid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
